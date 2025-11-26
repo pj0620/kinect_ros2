959ea6aa2fc7a7b9750bb4f4e400244b494bd2e6
pj <pjm7748@g.rit.edu>
2025-11-25 22:42:16 -0500

    fix: increase timer interval for improved performance

--- src/kinect_ros2_component.cpp
+++ src/kinect_ros2_component.cpp
@@ -17,7 +17,7 @@ static bool _rgb_flag;
 KinectRosComponent::KinectRosComponent(const rclcpp::NodeOptions & options)
 : Node("kinect_ros2", options)
 {
-  timer_ = create_wall_timer(1ms, std::bind(&KinectRosComponent::timer_callback, this));
+  timer_ = create_wall_timer(10ms, std::bind(&KinectRosComponent::timer_callback, this));
   
   std::string pkg_share = ament_index_cpp::get_package_share_directory("kinect_ros2");
 
