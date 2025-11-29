2b598b84bfdc66c9a9506eedd85ca5f153cac066
pj <pjm7748@g.rit.edu>
2025-11-28 19:49:20 -0500

    fix: switch from MultiThreadedExecutor to SingleThreadedExecutor for simplified execution

--- src/kinect_ros2_node.cpp
+++ src/kinect_ros2_node.cpp
@@ -43,7 +43,9 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
 
-  rclcpp::executors::MultiThreadedExecutor exec(rclcpp::ExecutorOptions(), 2);
+  // rclcpp::executors::MultiThreadedExecutor exec(rclcpp::ExecutorOptions(), 2);
+  rclcpp::executors::SingleThreadedExecutor exec;
+
   rclcpp::NodeOptions options;
   options.use_intra_process_comms(true);
 
