72f14b9b388bb7fa1a8345edabf292c4b603a133
pj <pjm7748@g.rit.edu>
2025-12-27 19:47:38 -0500

    fix clock issue

--- src/kinect_ros2_component.cpp
+++ src/kinect_ros2_component.cpp
@@ -17,11 +17,7 @@ static bool _rgb_flag;
 KinectRosComponent::KinectRosComponent(const rclcpp::NodeOptions & options)
 : Node("kinect_ros2", options)
 {
-<<<<<<< Updated upstream
-  timer_ = create_wall_timer(1ms, std::bind(&KinectRosComponent::timer_callback, this));
-=======
-  timer_ = create_wall_timer(100ms, std::bind(&KinectRosComponent::timer_callback, this));
->>>>>>> Stashed changes
+  timer_ = create_wall_timer(10ms, std::bind(&KinectRosComponent::timer_callback, this));
   
   std::string pkg_share = ament_index_cpp::get_package_share_directory("kinect_ros2");
 
